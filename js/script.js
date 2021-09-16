1df63ef06bff9e22b058e349f80549cbeeb789f4
Harmew <dann.ew@outlook.com>
2021-09-16 14:47:09 -0300

    Accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from "./modules/scroll-suave.js";
+import Accordion from "./modules/accordion.js";
 
-import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -12,10 +12,12 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init()
+scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
 
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
