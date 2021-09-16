3ee9fcf4fca5c49a6f34340aa357f4659cbef5f9
Harmew <dann.ew@outlook.com>
2021-09-16 15:23:23 -0300

    modal refatorado com clas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
+import Modal from "./modules/modal.js";
 
-import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -20,8 +20,10 @@ accordion.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init()
 
-initModal();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
