d5d5395e599399021035d9202a0d687dc60f5e3a
Harmew <dann.ew@outlook.com>
2021-09-16 19:00:02 -0300

    dropdown menu refatorado

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ import Tooltip from "./modules/tooltip.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollAnima from "./modules/scroll-anima.js";
+import DropdownMenu from "./modules/dropdown-menu.js";
 
 
-import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 
@@ -37,19 +37,13 @@ tooltip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
 scrollAnima.init()
 
-
-
+const dropdownMenu = new DropdownMenu("[data-dropdown]")
+dropdownMenu.init()
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
 
 
-
-
-
-
-
-initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
