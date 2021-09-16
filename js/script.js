590531f71f047a893b9fe61d10b643eaae681b6e
Harmew <dann.ew@outlook.com>
2021-09-16 15:51:26 -0300

    Tooltop refatorada

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
+import Tooltip from "./modules/tooltip.js";
 
-import initTooltip from "./modules/tooltip.js";
+
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
@@ -23,8 +24,10 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init()
 
+const tooltip = new Tooltip("[data-tooltip]");
+tooltip.init()
 
-initTooltip();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
